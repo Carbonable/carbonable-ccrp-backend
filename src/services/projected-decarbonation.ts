dfa8446d8f5f05a316104703268a0b86783115b8
Valentin Dosimont <valentin@pupucecorp.com>
2023-08-15 11:21:45 +0200

    feat: add carbonable data fixtures

--- src/services/projected-decarbonation.ts
+++ src/services/projected-decarbonation.ts
@@ -25,7 +25,18 @@ type ProjectedDecarbonationWithPagination = {
     }
 };
 
+type GraphDataKv = {
+    key: string,
+    value: number,
+};
+type ProjectedDecarbonationGraph = DecarbonationVintage & {
+    emission: number,
+    target: number
+    data: GraphDataKv[],
+}
+
 type DataFilter = { filter?: string, page?: number, count?: number };
+
 
 @Injectable()
 export class ProjectedDecarbonationService {
@@ -33,9 +44,35 @@ export class ProjectedDecarbonationService {
 
     constructor(private prisma: PrismaService) { }
 
-    async get(filter: string): Promise<ProjectedDecarbonation[]> {
-        return (await this.getData({ filter: filter })).data;
+    async get(filter: string): Promise<ProjectedDecarbonationGraph[]> {
+        let years = await this.prisma.$queryRaw<DecarbonationVintage[]>`
+SELECT
+    ce.year,
+    SUM(ce.emission) as emissions,
+    SUM(ce.target) / COUNT(ce.id) as target
+FROM company_emission ce
+INNER JOIN company c on c.id = ce.company_id
+INNER JOIN projects p on p.company_id = c.id
+GROUP BY ce.year ORDER BY ce.year
+;
+        `;
+
+        let computedYears = [];
+        for (let year of years) {
+
+            let cc_data = await this.resolveFilterQuery(filter, year.year);
+
+            computedYears = [...computedYears, {
+                ...year,
+                emissions: parseInt(year.emissions),
+                target: parseInt(year.target),
+                data: cc_data,
+            }];
+        }
+
+        return computedYears;
     }
+
     async getTable(pagination: PaginationDTO): Promise<ProjectedDecarbonationWithPagination> {
         return await this.getData({ ...pagination });
     }
@@ -46,12 +83,13 @@ export class ProjectedDecarbonationService {
         let offset = (page - 1) * count;
         let years = await this.prisma.$queryRaw<DecarbonationVintage[]>`
 SELECT
-    pc.year,
-    SUM(pc.emission) as emissions,
-    SUM(pc.target) / COUNT(pc.id) as target
-FROM project_configuration pc
-INNER JOIN projects p on pc.project_id = p.id
-GROUP BY pc.year ORDER BY pc.year LIMIT ${count} OFFSET ${offset}
+    ce.year,
+    SUM(ce.emission) as emissions,
+    SUM(ce.target) / COUNT(ce.id) as target
+FROM company_emission ce
+INNER JOIN company c on c.id = ce.company_id
+INNER JOIN projects p on p.company_id = c.id
+GROUP BY ce.year ORDER BY ce.year LIMIT ${count} OFFSET ${offset}
 ;
         `;
 
@@ -82,19 +120,81 @@ SELECT
             }];
         }
 
-        let project_configuration_count = await this.prisma.$queryRaw<{ count: number }[]>`
-SELECT COUNT(pc.year)
-FROM project_configuration pc
-INNER JOIN projects p on pc.project_id = p.id
-GROUP BY pc.year
+        let company_emission_count = await this.prisma.$queryRaw<{ count: number }[]>`
+SELECT COUNT(ce.year)
+FROM company_emission ce
+INNER JOIN company c on c.id = ce.company_id
+INNER JOIN projects p on p.company_id = c.id
+GROUP BY ce.year
         `;
         let paginationObject = {
-            max_page: Math.ceil(project_configuration_count.length / count),
+            max_page: Math.ceil(company_emission_count.length / count),
             page_number: page,
             count: count,
         };
 
 
         return { data: computedYears, pagination: paginationObject };
+    }
+
+    async resolveFilterQuery(filter: string, year: string) {
+        let filters = {
+            "PROJECT_TYPE": this.resolveProject.bind(this),
+            "OFFSET_TYPE": this.resolveOffset.bind(this),
+            "INVESTMENT_TYPE": this.resolveInvestment.bind(this),
+        };
+        let filterFn = filters[filter];
+        if (!filterFn) {
+            throw new Error('Invalid filter provided');
+        }
+
+        return await filterFn(year);
+    }
+
+    async resolveProject(year: string) {
+        let cc_data = await this.prisma.$queryRaw<{ redd_plus_count: number, arr_count: number }[]>`
+SELECT
+    (SELECT COUNT(cc.id) FROM carbon_credits cc where cc.type = 'CONCERVATION' and cc.vintage = ${year}) as redd_plus_count,
+    (SELECT COUNT(cc.id) FROM carbon_credits cc where cc.type = 'RESTORATION' and cc.vintage = ${year}) as arr_count
+;
+            `;
+
+        let value = cc_data.pop();
+
+        return [
+            { key: 'REDD+', value: parseInt(value.redd_plus_count.toString()) },
+            { key: 'ARR', value: parseInt(value.arr_count.toString()) },
+        ];
+    }
+
+    async resolveOffset(year: string) {
+        let currentYear = new Date().getFullYear();
+        let cc_data = await this.prisma.$queryRaw<{ ex_post_count: number, ex_ante_count: number }[]>`
+SELECT
+    (SELECT COUNT(cc.id) FROM carbon_credits cc where cast(cc.vintage as int) <= ${currentYear} and cc.vintage = ${year}) as ex_post_count,
+    (SELECT COUNT(cc.id) FROM carbon_credits cc where cast(cc.vintage as int) > ${currentYear} and cc.vintage = ${year}) as ex_ante_count
+;
+            `;
+
+        let value = cc_data.pop();
+
+        return [
+            { key: 'ExPost', value: parseInt(value.ex_post_count.toString()) },
+            { key: 'ExAnte', value: parseInt(value.ex_ante_count.toString()) },
+        ];
+    }
+
+    async resolveInvestment(year: string) {
+        let cc_data = await this.prisma.$queryRaw<{ direct_purchase_count: number, forward_finance_count: number }[]>`
+SELECT
+    (SELECT COUNT(cc.id) FROM carbon_credits cc INNER JOIN projects p on p.id = cc.project_id where p.origin = 'DIRECT_PURCHASE' and cc.vintage = ${year}) as direct_purchase_count,
+    (SELECT COUNT(cc.id) FROM carbon_credits cc INNER JOIN projects p on p.id = cc.project_id where p.origin = 'FORWARD_FINANCE' and cc.vintage = ${year}) as forward_finance_count
+;
+            `;
+        let value = cc_data.pop();
+        return [
+            { key: 'Forward Finance', value: parseInt(value.forward_finance_count.toString()) },
+            { key: 'Direct Purchase', value: parseInt(value.direct_purchase_count.toString()) }
+        ];
     }
 }
