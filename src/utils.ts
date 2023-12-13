172423800f89b64cc601e8c76b04f8bafdfea945
Valentin Dosimont <valentin@pupucecorp.com>
2023-12-13 16:24:48 +0100

    feature: add pagination

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
+class PaginationDTO {
+  page = 1;
+  count = 10;
+}
+
 type FormatStringArgs = {
   value: string;
   suffix?: string;
@@ -8,5 +13,24 @@ export default class Utils {
     return `${prefix ? prefix + ' ' : ''}${parseInt(value).toLocaleString(
       'fr',
     )}${suffix ? ' ' + suffix : ''}`;
+  }
+
+  static paginate(data: any[], pagination: any) {
+    if (!pagination) {
+      pagination = new PaginationDTO();
+    }
+
+    const total_page = Math.ceil(data.length / pagination.count);
+    return {
+      data: data.slice(
+        (pagination.page - 1) * pagination.count,
+        pagination.page * pagination.count,
+      ),
+      page_info: {
+        total_page,
+        has_next_page: pagination.page < total_page,
+        has_previous_page: pagination.page > 1,
+      },
+    };
   }
 }
