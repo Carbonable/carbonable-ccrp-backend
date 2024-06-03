a338599229f9dd5d3618afdc9162e279a7b49db6
François Schuers <francois.schuers@gmail.com>
2024-06-03 11:07:53 +0200

    fix bioconception absorption curve

--- src/console/fixtures-data/bio-concepcion.ts
+++ src/console/fixtures-data/bio-concepcion.ts
@@ -1,6 +1,6 @@
 export const BioConcepcionAZCurvePoints = [
   { timestamp: 1746540000, absorption: 7000000, purchasedPrice: 140 },
-  { timestamp: 1778076000, absorption: 1000000, purchasedPrice: 140 },
+  { timestamp: 1778076000, absorption: 10000000, purchasedPrice: 140 },
   { timestamp: 1809612000, absorption: 14000000, purchasedPrice: 140 },
   { timestamp: 1841234400, absorption: 20000000, purchasedPrice: 140 },
   { timestamp: 1872770400, absorption: 20000000, purchasedPrice: 140 },
