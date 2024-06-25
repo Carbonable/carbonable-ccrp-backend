cab692fa6cb6be999c34f66fb68f3f2b57d708eb
LouGel <l.gelinet@protonmail.ch>
2024-06-25 11:10:49 +0200

    fix: authcontroller specs missing data

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { JwtService } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { PrismaService } from '../infrastructure/prisma.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +11,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [JwtService, AuthService, UsersService, PrismaService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
