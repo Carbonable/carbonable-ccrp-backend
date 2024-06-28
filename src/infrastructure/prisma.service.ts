4bd1ae3662a70b999d8e3a37d820e403c6a416ea
Valentin Dosimont <valentin@pupucecorp.com>
2024-06-28 11:49:31 +0200

    feat: add integration tests

--- src/infrastructure/prisma.service.ts
+++ src/infrastructure/prisma.service.ts
@@ -3,30 +3,15 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  // constructor() {
-  //   super({
-  //     log: [
-  //       {
-  //         emit: 'event',
-  //         level: 'query',
-  //       },
-  //     ],
-  //   });
-  // }
-  async onModuleInit() {
-    await this.$connect();
-    // // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // // @ts-ignore
-    // this.$on('query', async (e) => {
-    //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //   // @ts-ignore
-    //   console.log(`${e.query} ${e.params}`);
-    // });
+  constructor() {
+    super();
   }
 
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
