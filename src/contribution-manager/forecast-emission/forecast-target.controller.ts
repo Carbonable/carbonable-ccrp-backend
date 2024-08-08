40ce643ec0ddd2ba5c21053d0e20e57735aa13ad
LouGel <l.gelinet@protonmail.ch>
2024-08-08 13:27:42 +0200

    chore: forecast-target/feature:generic forecast upload

--- src/contribution-manager/forecast-emission/forecast-target.controller.ts
+++ src/contribution-manager/forecast-emission/forecast-target.controller.ts
@@ -8,30 +8,30 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { Logger } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ForecastService } from './forecast.service';
+import { ForecastType } from './types';
 import { Roles } from '../../roles/roles.decorator';
 import { Role } from '../../roles/role.enum';
-import { ForecastType } from './types';
 
-@ApiTags('forecast-emission')
-@Controller('forecast-emission')
-export class ForecastEmissionController {
-  private readonly logger = new Logger(ForecastEmissionController.name);
+@ApiTags('forecast-target')
+@Controller('forecast-target')
+export class ForecastTargetController {
+  private readonly logger = new Logger(ForecastTargetController.name);
 
   constructor(private carbonCreditService: ForecastService) {}
 
   @Roles(Role.Admin)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  @ApiOperation({ summary: 'Upload Forecast Emission CSV file' })
+  @ApiOperation({ summary: 'Upload Forecast Target CSV file' })
   @ApiResponse({ status: 201, description: 'File successfully processed.' })
   @ApiResponse({ status: 400, description: 'Invalid file format.' })
-  async uploadForecastEmissionsCsv(
+  async uploadForecastTargetsCsv(
     @UploadedFile() file: Express.Multer.File,
   ): Promise<{ message: string }> {
     this.logger.debug(`File uploaded: ${file.originalname}`);
     return await this.carbonCreditService.processCsv(
       file.buffer,
-      ForecastType.EMISSION,
+      ForecastType.TARGET,
     );
   }
 }
