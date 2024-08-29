349cce01d1dbcfecbfa89100f0059e3b381e2ef2
François Schuers <francois.schuers@gmail.com>
2024-08-29 15:19:48 +0200

    fix: missing import

--- src/console/console.module.ts
+++ src/console/console.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { BuildFixturesCommand } from './build-fixtures';
 import { InfrastructureModule } from '../infrastructure/infrastructure.module';
 import { FlushRedisCommand } from './flush-redis';
-import { Vintage } from 'src/domain/portfolio';
 
 @Module({
   providers: [BuildFixturesCommand, FlushRedisCommand],
   exports: [BuildFixturesCommand, FlushRedisCommand],
-  imports: [InfrastructureModule, Vintage],
+  imports: [InfrastructureModule],
 })
 export class ConsoleModule {}
