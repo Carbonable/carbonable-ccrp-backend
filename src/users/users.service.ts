d173eb45a23c24dad9530172b390c81e536a887d
LouGel <l.gelinet@protonmail.ch>
2024-08-31 11:01:32 +0200

    clean: removed user service

--- src/users/users.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Logger } from '@nestjs/common';
-
-@Injectable()
-export class UsersService {
-  private readonly logger = new Logger(UsersService.name);
-}
